3f412fd3d0c3924588530deb9a69e01690e466ba
lbatschelet <78535538+lbatschelet@users.noreply.github.com>
2026-05-07 09:05:11 +0000

    Deploying to deploy/feelvonroll from @ lbatschelet/wellspace@524e69cde947f5505dc23d0cdb100d82b33e933d 🚀

--- admin/assets/footer-DGTr_Wnb.js
+++ admin/assets/footer-DGTr_Wnb.js
@@ -1,4 +1,4 @@
-import{g as o}from"./webappBase-C-OK_qxg.js";import"./config-DvzYGSBB.js";const t=o()||"",s=t?`${t}/issue/?source=admin`:"#";function c(){const e=document.createElement("footer");e.className="app-footer";const r=new Date().getFullYear(),a="v1.0.0-beta.3";return e.innerHTML=`
+import{g as o}from"./webappBase-C-OK_qxg.js";import"./config-DvzYGSBB.js";const t=o()||"",s=t?`${t}/issue/?source=admin`:"#";function c(){const e=document.createElement("footer");e.className="app-footer";const r=new Date().getFullYear(),a="v1.0.0-beta.4";return e.innerHTML=`
     <div class="footer-left">
       <span class="footer-credit">
         © ${r}
