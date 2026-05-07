73cc14a559e0621c9a91e252dde18a240c2b86b3
lbatschelet <78535538+lbatschelet@users.noreply.github.com>
2026-05-07 10:39:03 +0000

    Deploying to deploy/feelvonroll from @ lbatschelet/wellspace@77313577f47fed0eb39fe1ac2ae0badf32cf2574 🚀

--- admin/assets/footer-BmsG0d7X.js
+++ admin/assets/footer-BmsG0d7X.js
@@ -1,4 +1,4 @@
-import{g as o}from"./webappBase-C-OK_qxg.js";import"./config-DvzYGSBB.js";const t=o()||"",s=t?`${t}/feedback/?source=admin`:"#";function c(){const e=document.createElement("footer");e.className="app-footer";const r=new Date().getFullYear(),a="v1.0.0-beta.11-1-g84f915b";return e.innerHTML=`
+import{g as o}from"./webappBase-C-OK_qxg.js";import"./config-DvzYGSBB.js";const t=o()||"",s=t?`${t}/feedback/?source=admin`:"#";function c(){const e=document.createElement("footer");e.className="app-footer";const r=new Date().getFullYear(),a="v1.0.0-beta.12";return e.innerHTML=`
     <div class="footer-left">
       <span class="footer-credit">
         © ${r}
