4615dea05baab51c3271bd64dcc8d9914ccd4deb
lbatschelet <78535538+lbatschelet@users.noreply.github.com>
2026-05-07 15:36:05 +0000

    Deploying to deploy/feelvonroll from @ lbatschelet/wellspace@5ea8b75b952835417a42ab33af79aacc68ac4844 🚀

---
+++ api/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wellspace/api',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '5ea8b75b952835417a42ab33af79aacc68ac4844',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.12.0',
+            'version' => '6.12.0.0',
+            'reference' => 'd1ac35d784bf9f5e61b424901d5a014967f15b12',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wellspace/api' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '5ea8b75b952835417a42ab33af79aacc68ac4844',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
