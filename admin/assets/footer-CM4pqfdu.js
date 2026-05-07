68cce8b7068b051670af897fafbb6712842874af
lbatschelet <78535538+lbatschelet@users.noreply.github.com>
2026-05-07 11:31:00 +0000

    Deploying to deploy/wohlopti from @ lbatschelet/wellspace@028da6c28fa30c83e5783bfbcb901c678e8a1540 🚀

--- admin/assets/footer-CM4pqfdu.js
+++ admin/assets/footer-CM4pqfdu.js
@@ -1,4 +1,4 @@
-import{g as o}from"./webappBase-BtC6Se1U.js";import"./config-BJ-Z3s_A.js";const t=o()||"",s=t?`${t}/feedback/?source=admin`:"#";function c(){const e=document.createElement("footer");e.className="app-footer";const r=new Date().getFullYear(),a="v1.0.0-beta.13";return e.innerHTML=`
+import{g as o}from"./webappBase-BtC6Se1U.js";import"./config-BJ-Z3s_A.js";const t=o()||"",s=t?`${t}/feedback/?source=admin`:"#";function c(){const e=document.createElement("footer");e.className="app-footer";const r=new Date().getFullYear(),a="v1.0.0-beta.14";return e.innerHTML=`
     <div class="footer-left">
       <span class="footer-credit">
         © ${r}
